9d55854c775d3e29cfd5f7d0bc32cbe00fee241f
Bhavitha Gottimukkula <bhavitha.gottimukkula@fissionlabs.in>
2018-01-11 15:44:32 +0530

    Splash implementation

--- app/src/main/java/com/example/bhavithagottimukkula/login_java/SplashActivity.java
+++ app/src/main/java/com/example/bhavithagottimukkula/login_java/SplashActivity.java
@@ -1,35 +1,51 @@
 package com.example.bhavithagottimukkula.login_java;
 
 import android.content.Intent;
-import android.os.Handler;
-import android.os.SystemClock;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import java.util.concurrent.TimeUnit;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private Handler mHandler = new Handler();
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+
+            Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(intent);
+            if(isFinishing()) {
+                finish();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
 
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }, 500);
+        setContentView(R.layout.activity_splash);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mHandler.postDelayed(mRunnable, 5000);
 
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mHandler.removeCallbacks(mRunnable);
+    }
 
-
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHandler.removeCallbacks(mRunnable);
+    }
 }
